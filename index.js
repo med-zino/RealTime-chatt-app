b254b29acc6c075d4e2bb63c93b9ffb6ce8d72fb
med-zino <medzino85@gmail.com>
2022-07-14 17:16:29 +0100

    adding users list and chatroom name

--- index.js
+++ index.js
@@ -43,6 +43,12 @@ io.on('connection', (socket) => {
         formatMessage(botName, `${username} has joined the chatt`)
       )
 
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    })
+
     socket.on('disconnect', () => {
       const user = userLeave(socket.id)
       if (user) {
@@ -50,6 +56,12 @@ io.on('connection', (socket) => {
           'message',
           formatMessage(botName, `${user.username} has left the chat`)
         )
+
+        // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+          room: user.room,
+          users: getRoomUsers(user.room),
+        })
       }
     })
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 const chatForm = document.getElementById('chat-form')
 const socket = io()
 const chatMessages = document.querySelector('.chat-messages')
+const roomName = document.getElementById('room-name')
+const userList = document.getElementById('users')
 
 // Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
@@ -9,6 +11,11 @@ const { username, room } = Qs.parse(location.search, {
 
 // Join chatroom
 socket.emit('joinRoom', { username, room })
+
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room)
+  outputUsers(users)
+})
 
 socket.on('message', (message) => {
   console.log(message)
@@ -36,4 +43,18 @@ function outputMessage(message) {
 
   const mssg = document.getElementById('msg')
   mssg.value = ''
+}
+
+function outputRoomName(room) {
+  roomName.innerText = room
+}
+
+// Add users to DOM
+function outputUsers(users) {
+  userList.innerHTML = ''
+  users.forEach((user) => {
+    const li = document.createElement('li')
+    li.innerText = user.username
+    userList.appendChild(li)
+  })
 }
